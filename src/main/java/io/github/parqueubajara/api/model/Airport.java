2349639d2af922b823d7725f6f589777a340c5f6
Felipe Levi <felipegpt19@gmail.com>
2026-04-15 20:58:24 -0300

    feat: implement Airport and Contact entities closes #12

---
+++ src/main/java/io/github/parqueubajara/api/model/Airport.java
@@ -0,0 +1,43 @@
+package io.github.parqueubajara.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_airport")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Airport {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "iata_code")
+    private String iataCode;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "distance_km")
+    private Double distanceKm;
+
+    @Column(name = "estimated_time_minutes")
+    private Integer estimatedTimeMinutes;
+
+    @Column(name = "route_description")
+    private String routeDescription;
+}
---
+++ src/main/java/io/github/parqueubajara/api/model/Contacts.java
@@ -0,0 +1,37 @@
+package io.github.parqueubajara.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Contacts {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "description")
+    private String description;
+}
